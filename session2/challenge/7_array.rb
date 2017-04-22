f6fc792a1c814ac4c3b7641c1b34b8599ae96b5d
toby <toby.hw@live.co.uk>
2017-04-22 01:02:04 +0100

    Session2 Challenge 7+8 completed

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -8,3 +8,9 @@
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(string)
+	index = -1
+	word_array = string.gsub(/[^A-z0-9'’]/, " ").split(" ")
+	word_array.reject { |e| (index+=1).odd? }
+end
--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,21 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+# def got_three?(array)
+#	array.each_cons(3) { |a,b,c| 
+#	if a == ( b && c )
+#		return true
+#	else 
+#		return false
+#	end
+#	}
+# end
+
+
+def got_three?(array)
+	array.each_cons(3) do |a,b,c| 
+		return true if a ==b && b == c 
+	end
+	false
+end
