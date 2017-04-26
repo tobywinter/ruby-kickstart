9c1bb72514e8b70a7415e5603fe6755c72abf160
toby <toby.hw@live.co.uk>
2017-04-26 17:51:58 +0100

    Session3 Challenge 16 & 17 complete

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -2,8 +2,27 @@
 # If you don't know what that is, go check out the previous problem.
 #
 # EXAMPLES:
-# head = {:data => 1, :next => nil}
-# head = {:data => 2, :next => head}
+ head = {:data => 1, :next => nil}
+ head = {:data => 2, :next => head}
 #
-# print_list_in_reverse head   # >> "1\n2\n"
 
+def print_list(list)
+	while list
+		puts list[:data]
+		list = list[:next]
+	end 
+end
+
+
+def print_list_in_reverse(list)
+	list_arr = []
+	while list
+		list_arr << (list[:data])
+		list = list[:next]
+	end
+	list_arr.reverse!.each { |i| p i  }
+
+end
+
+
+print_list_in_reverse head   # >> "1\n2\n"
